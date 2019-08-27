afad0c6ed17a4207743bfeec187e7672c489b673
Teran <5717131+capk1rk@users.noreply.github.com>
2019-08-27 16:14:46 -0600

    Testing permutations

---
+++ Problem 24/solve24.py
@@ -0,0 +1,15 @@
+def perm(digits, perms, ):
+    if len(digits) > 2:
+        perm()
+
+
+def perm(i):
+    chars = []
+    for n in range(i):
+        chars.append(str(n))
+    print(chars)
+
+    perms = set()
+
+
+perm(3)
